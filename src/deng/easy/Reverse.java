1eb6b926300777b51c87568c1f2ea4fa6c7f769e
lt14 <592737616@qq.com>
2018-05-28 17:25:04 +0800

    更新Reverse和TwoSum解法

--- src/deng/easy/Reverse.java
+++ src/deng/easy/Reverse.java
@@ -9,20 +9,39 @@ package deng.easy;
  * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2^31,  2^31 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
  */
 public class Reverse {
+    // 复杂度n
+//    public int reverse(int x) {
+//        StringBuilder reverseStr = new StringBuilder();
+//        if (x < 0) {
+//            reverseStr.append("-");
+//            x = -x;
+//        }
+//
+//        String str = String.valueOf(x);
+//        for (int i = str.length() - 1; i >= 0; i--) {
+//            reverseStr.append(str.charAt(i));
+//        }
+//
+//        try {
+//            return Integer.parseInt(reverseStr.toString());
+//        } catch (java.lang.NumberFormatException e) {
+//            return 0;
+//        }
+//    }
+
+    // n/2
     public int reverse(int x) {
-        StringBuilder reverseStr = new StringBuilder();
-        if (x < 0) {
-            reverseStr.append("-");
-            x = -x;
+        char[] chars = x >= 0 ? String.valueOf(x).toCharArray() : String.valueOf(-x).toCharArray();
+
+        for (int i = 0; i < chars.length / 2; i++) {
+            char temp = chars[i];
+            chars[i] = chars[chars.length - 1 - i];
+            chars[chars.length - 1 - i] = temp;
         }
 
-        String str = String.valueOf(x);
-        for (int i = str.length() - 1; i >= 0; i--) {
-            reverseStr.append(str.charAt(i));
-        }
-
+        String reverseStr = x >= 0 ? new String(chars) : "-" + new String(chars);
         try {
-            return Integer.parseInt(reverseStr.toString());
+            return Integer.parseInt(reverseStr);
         } catch (java.lang.NumberFormatException e) {
             return 0;
         }
--- src/deng/easy/TwoSum.java
+++ src/deng/easy/TwoSum.java
@@ -1,5 +1,8 @@
 package deng.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode-cn.com/problems/two-sum/description/
  * 给定 nums = [2, 7, 11, 15], target = 9
@@ -7,16 +10,36 @@ package deng.easy;
  * 所以返回 [0, 1]
  */
 public class TwoSum {
+    // 复杂度O(n^2)
+//    public int[] twoSum(int[] nums, int target) {
+//        for (int m = 0; m < nums.length - 1; m++) {
+//            for (int n = m + 1; n < nums.length; n++) {
+//                int sum = nums[m] + nums[n];
+//                if (sum == target) {
+//                    return new int[]{m, n};
+//                }
+//            }
+//        }
+//
+//        return null;
+//    }
+
+    // 复杂度O(n)
     public int[] twoSum(int[] nums, int target) {
-        for (int m = 0; m < nums.length - 1; m++) {
-            for (int n = m + 1; n < nums.length; n++) {
-                int sum = nums[m] + nums[n];
-                if (sum == target) {
-                    return new int[]{m, n};
-                }
-            }
+        Map<Integer, Integer> map = new HashMap();
+        int[] a = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            map.put(target - nums[i], i);
         }
 
-        return null;
+        for (int i = 0; i < nums.length; i++) {
+            //可能存在一个数x,满足x*2=target，i!=map.get(nums[i])可以避免
+            if (map.containsKey(nums[i]) == true && i != map.get(nums[i])) {
+                a[0] = i < map.get(nums[i]) ? i : map.get(nums[i]);
+                a[1] = i > map.get(nums[i]) ? i : map.get(nums[i]);
+                break;
+            }
+        }
+        return a;
     }
 }
