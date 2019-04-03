caf7014d68195ecbd3e517131b8f5f865c19bfcd
lt14 <592737616@qq.com>
2019-04-03 23:26:28 +0800

    更新题目描述；删除排序链表的重复元素

--- src/deng/easy/MergeTwoSortedLists.java
+++ src/deng/easy/MergeTwoSortedLists.java
@@ -3,6 +3,10 @@ package deng.easy;
 import deng.common.ListNode;
 
 /**
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ *
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
  * @author deng
  * @date 2019/04/03
  */
